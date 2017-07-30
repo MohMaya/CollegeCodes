c3beb5aa688e59ef228d0b8f61e327da29ba3bdd
Shivanshu Chaudhary <shivanshuchaudhary97@gmail.com>
2017-07-30 12:03:42 +0530

    Finished cipherDemo.py for RSA encryption

--- 5th Semester/Computer Networking Lab/Python3/05 RSA Encryption/cipherDemo.py
+++ 5th Semester/Computer Networking Lab/Python3/05 RSA Encryption/cipherDemo.py
@@ -1,24 +1,93 @@
-#Demo for XXXX Cipher By Shivanshu Chaudhary aka MOHMAYA
+#Demo for RSA Encryption By Shivanshu Chaudhary aka MOHMAYA
 
 
-def encryptor(originalMessage):        #Function for encrypting the originalMessage
-    encryptedMessage = ""
+from math import sqrt
+
+
+def encryptor(originalMessage,publicKey):        #Function for encrypting the originalMessage
+    encryptedMessage = pow(originalMessage,publicKey[1]) % publicKey[0]
     #logic for encryption
-
     return encryptedMessage
 
 
-def decryptor(encryptedMessage):        #Function for decrypting the encryptedMessage
-    originalMessage = ""
+def decryptor(encryptedMessage,privateKey):        #Function for decrypting the encryptedMessage
+    originalMessage = pow(encryptedMesasge,privateKey[1]) % privateKey[0]
     #logic for decrypting
-
     return originalMessage
 
 
-userInput = input("\nPlease Enter Your Desired Message : ")
-encryptedMesasge = encryptor(userInput)
+def isPrime(n):
+    for i in range(2,int(sqrt(n))):
+        if n%i == 0:
+            return False
+    return True
 
-print("\nThe Encypted Message is  : "+encryptedMesasge)
 
-originalMessage = decryptor(encryptedMesasge)
-print("\nThe Decrypted Message is : "+originalMessage)
+def isCoprime(e,fi):
+    if e < fi:
+        for i in range(2,e+1):
+            if(fi%i == 0) and (e%i == 0):
+                return False
+    elif e>fi:
+        for i in range(2,fi+1):
+            if(e%i == 0) and (fi%i == 0):
+                return False
+    else:
+        return False
+    return True
+
+
+def dCalculator(e,fi):
+    k=0
+    d=0
+    while True:
+        if (1+(k*fi))%e == 0:
+            d = (1+(k*fi))//e
+            return d
+        k+=1
+    return 0
+
+
+while True:
+    primeNumber1 = int(input("Enter First Prime Number : "))
+    if isPrime(primeNumber1):
+        break
+    print("Not A Prime Number")
+
+
+while True:
+    primeNumber2 = int(input("Enter The Second Prime Number : "))
+    if isPrime(primeNumber2):
+        break
+    print("Not A Prime Number")
+
+
+modulus = primeNumber1 * primeNumber2
+
+totient = (primeNumber1 - 1) * (primeNumber2 - 1)
+
+while True:
+        publicKeyExponent = int(input("Enter The Public Key Exponent Of Your Message : "))
+        if isCoprime(publicKeyExponent,totient):
+            break
+        print("Invalid Key!!!")
+
+
+privateKeyExponent = dCalculator(publicKeyExponent,totient)
+
+
+publicKey = (modulus,publicKeyExponent)
+privateKey = (modulus,privateKeyExponent)
+print("Public Key : ",publicKey)
+print("Private Key : ",privateKey)
+
+
+
+
+userInput = int(input("\nPlease Enter Your Desired Numerical Message : "))
+
+encryptedMesasge = encryptor(userInput,publicKey)
+print("\nThe Encypted Message is  : "+str(encryptedMesasge))
+
+originalMessage = decryptor(encryptedMesasge,privateKey)
+print("\nThe Decrypted Message is : "+str(originalMessage))
